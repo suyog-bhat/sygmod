da698ed490f23d66685c71ec0a0ee3d70d453d4c
Suyog Bhat <suyog@Suyogs-MacBook-Pro.local>
2021-08-18 15:05:25 +0530

    addnum patch

--- addpack/add.go
+++ addpack/add.go
@@ -1,6 +1,12 @@
 package addpack
 
-func Addnums(a int,b int) int{
-	return a+b
+import "errors"
+
+func Addnums(a int,b int) (int,error){
+	if a<0{
+		err:=errors.New("negative number")
+		return 0,err
+	}
+	return a+b,nil
 
 }
